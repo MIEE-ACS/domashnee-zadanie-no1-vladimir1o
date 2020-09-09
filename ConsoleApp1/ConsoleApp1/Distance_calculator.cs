643a6061a50a04c49bf811fd896ccdbf9d84d41e
vladimir1o <54781268+vladimir1o@users.noreply.github.com>
2020-09-09 19:49:58 +0300

    ДЗ 1

---
+++ ConsoleApp1/ConsoleApp1/Distance_calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Distance_calculator
+    {
+        static void Main(string[] args)
+        {
+            int x1, y1, x2, y2;
+            double distance;
+
+            try
+            {
+                Console.Write("Введите координату x первой точки: ");
+                x1 = int.Parse(Console.ReadLine());
+                Console.Write("Введите координату y первой точки: ");
+                y1 = int.Parse(Console.ReadLine());
+                Console.Write("Введите координату x второй точки: ");
+                x2 = int.Parse(Console.ReadLine());
+                Console.Write("Введите координату y второй точки: ");
+                y2 = int.Parse(Console.ReadLine());
+
+                distance = Math.Sqrt(Math.Pow((double)x2 - (double)x1, 2) + Math.Pow((double)y2 - (double)y1, 2));
+
+                Console.WriteLine($"Расстояние между точками: {Math.Round(distance, 3)}");
+            }
+            catch
+            {
+                Console.WriteLine("ОШИБКА! Неправильный ввод!");
+            }
+            
+
+           
+        }
+    }
+}
